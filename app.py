e1d0baced154b60f308a5054681f7ff36fbd719e
Curtis Su <chengjia.su@gmail.com>
2022-01-09 02:57:30 +0800

    add backend web and add new color

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os.path
 import psycopg2
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template, url_for, redirect, flash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from imgurpython import ImgurClient
 
 from linebot import (
@@ -21,14 +22,70 @@ from linebot.models import *
 app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
+app.secret_key = config['flask']['secret_key']
 
 line_bot_api = LineBotApi(os.environ.get("ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("SECRET"))
-client_id = config['imgur_api']['Client_ID']
-client_secret = config['imgur_api']['Client_Secret']
-album_id = config['imgur_api']['Album_ID']
-API_Get_Image = config['other_api']['API_Get_Image']
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'login'
+
+users = {'curtis': {'password': 'Mazda!cx30@6576'}}
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(test_user):
+    if test_user not in users:
+        return
+
+    user = User()
+    user.id = test_user
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    test_user = request.form.get('user_id')
+    if test_user not in users:
+        return
+
+    user = User()
+    user.id = test_user
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[test_user]['password']
+
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    
+    test_user = request.form['user_id']
+    if (test_user in users) and (request.form['password'] == users[test_user]['password']):
+        user = User()
+        user.id = test_user
+        login_user(user)
+        return redirect(url_for('home'))
+
+    flash('帳號或密碼錯誤!非版主請勿嘗試登入!')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    test_user = current_user.get_id()
+    logout_user()
+    return render_template('login.html')
+
+@app.route("/")
+@login_required
+def home():
+    return render_template("home.html")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -94,6 +151,8 @@ def get_color_fullname(color):
         return "飛梭銀"
     elif "棕" in color:
         return "鈦金棕"
+    elif "琉光" in color or "光金" in color:
+        return "琉光金"
     else:
         return None
 
@@ -144,7 +203,7 @@ def handle_message(event):
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
 
-    req_msg = str(event.message.text)
+    req_msg = str(event.message.text).strip()
         
     if req_msg.startswith("++") and req_msg[2:6].isnumeric():
         if ":" not in req_msg or "/" not in req_msg:
@@ -152,8 +211,8 @@ def handle_message(event):
         else:
             number = req_msg[2:6]
             pre_msg = req_msg.split("/")[0]
-            color_in = req_msg.split("/")[-1]
-            name = pre_msg.split(":")[-1]
+            color_in = req_msg.split("/")[-1].strip()
+            name = pre_msg.split(":")[-1].strip()
             color = get_color_fullname(color_in)
             if color is None:
                 reply_msg = TextSendMessage(text="車牌【{}】註冊失敗, 車色 {} 無法辨識".format(number, color_in))
@@ -184,7 +243,7 @@ def handle_message(event):
             reply_msg = TextSendMessage(text="刪除車牌格式錯誤. 格式應為:\n--[車號數字四位數]:[車主名]\n例如 --1234:彰化金城武")
         else:
             number = req_msg[2:6]
-            name = req_msg.split(":")[-1]
+            name = req_msg.split(":")[-1].strip()
             ret = unregister_car(number, name)
             if ret:
                 reply_msg = TextSendMessage(text="車牌【{}: {}】刪除成功".format(number, name))
@@ -194,7 +253,7 @@ def handle_message(event):
         return 0
 
     if req_msg.startswith("!!total"):
-        ret_text = "【已註冊車色統計資料】\n躍雪白:{}\n極粹白:{}\n極境灰:{}\n鋼鐵灰:{}\n御鉄黑:{}\n晶艷魂動紅:{}\n星燦藍:{}\n飛梭銀:{}\n鈦金棕:{}".format(query_color_number("躍雪白"),
+        ret_text = "【已註冊車色統計資料】\n躍雪白:{}\n極粹白:{}\n極境灰:{}\n鋼鐵灰:{}\n御鉄黑:{}\n晶艷魂動紅:{}\n星燦藍:{}\n飛梭銀:{}\n鈦金棕:{}\n琉光金:{}".format(query_color_number("躍雪白"),
                                                                                                                                                    query_color_number("極粹白"),
                                                                                                                                                    query_color_number("極境灰"),
                                                                                                                                                    query_color_number("鋼鐵灰"),
@@ -202,7 +261,8 @@ def handle_message(event):
                                                                                                                                                    query_color_number("晶艷魂動紅"),
                                                                                                                                                    query_color_number("星燦藍"),
                                                                                                                                                    query_color_number("飛梭銀"),
-                                                                                                                                                   query_color_number("鈦金棕"))
+                                                                                                                                                   query_color_number("鈦金棕"),
+                                                                                                                                                   query_color_number("琉光金"))
         reply_msg = TextSendMessage(text=ret_text)
         line_bot_api.reply_message(event.reply_token, reply_msg)
         return 0
